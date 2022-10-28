414a23810b7c5fca449dd4bb2da1cb47bc0fcf69
skdlyn <rafli.dwiferdiansyah01@gmail.com>
2022-10-28 13:07:21 +0700

    middleware

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -50,7 +50,7 @@
                     <a class="nav-link active" href="/about">About</a>
                     <a class="nav-link active" href="/project">Projects</a>
                     <a class="nav-link active" href="/contact">Contact</a>
-                    <a class="nav-link active" href="/login">Login</a>
+                    <input type="submit" class="btn btn-primary" value="Login">
                 </div>
             </div>
         </div>
